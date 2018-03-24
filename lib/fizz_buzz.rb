a933e34695005342d2b86fe105713b66b646e6ea
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-03-24 15:11:15 +0900

    Finished 1~3

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,23 @@
+def my_simplest_fizz_buzz(number)
+  if number % 15 == 0
+    'Fizz Buzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
+  end
+end
+
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+end
